210711aef642570536d81e65922e326116b4ec00
Piotr-Gawelda <piotr.gawelda@microsoft.wsei.edu.pl>
2022-03-04 18:57:55 +0100

    update

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -5,55 +5,98 @@ using System.Linq;
 using System.Threading.Tasks;
 using LibApp.Models;
 using LibApp.ViewModels;
+using LibApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibApp.Controllers
 {
     public class BooksController : Controller
     {
-        public IActionResult Random()
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
         {
-            var firstBook = new Book() { Name = "English dictionary" };
-
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"}
-            };
-
-
-            var viewModel = new RandomBookViewModel
-            {
-                Book = firstBook,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            _context = context;
         }
 
-        public IActionResult Edit(int bookId)
+        public IActionResult Index()
         {
-            return Content("id" + bookId);
+            var books = _context.Books
+                .Include(b => b.Genre)
+                .ToList();
+
+            return View(books);
         }
 
-        public IActionResult Index(int? pageIndex, string sortBy)
+        public IActionResult Details(int id)
         {
-            if (!pageIndex.HasValue)
+            var book = _context.Books
+                .Include(b => b.Genre)
+                .SingleOrDefault(b => b.Id == id);
+
+            return View(book);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.SingleOrDefault(b => b.Id == id);
+            if (book == null)
             {
-                pageIndex = 1;
+                return NotFound();
             }
 
-            if (String.IsNullOrEmpty(sortBy))
+            var viewModel = new BookFormViewModel
             {
-                sortBy = "Name";
+                Book = book,
+                Genres = _context.Genre.ToList()
+            };
+
+            return View("BookForm", viewModel);
+        }
+
+        public IActionResult New()
+        {
+            var viewModel = new BookFormViewModel
+            {
+                Genres = _context.Genre.ToList()
+            };
+
+            return View("BookForm", viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Save(Book book)
+        {
+            if (book.Id == 0)
+            {
+                book.DateAdded = DateTime.Now;
+                _context.Books.Add(book);
+            }
+            else
+            {
+                var bookInDb = _context.Books.Single(b => b.Id == book.Id);
+                bookInDb.Name = book.Name;
+                bookInDb.AuthorName = book.AuthorName;
+                bookInDb.GenreId = book.GenreId;
+                bookInDb.ReleaseDate = book.ReleaseDate;
+                bookInDb.DateAdded = book.DateAdded;
+                bookInDb.NumberInStock = book.NumberInStock;
             }
 
-            return Content($"pageindex={pageIndex}&sortBy={sortBy}");
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return RedirectToAction("Index", "Books");
         }
 
-        [Route("books/released/{year:regex(^\\d{{4}}$):min(1925):max(2021)}/{month:range(1,12)}")]
-        public IActionResult ByReleaseData(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+
+
+
     }
 }
